6e37492695297c9e6baa40ac66042408717f1383
Roshan <roshanprajapati9076@gmail.com>
2023-12-11 18:02:06 +0530

    first commit

--- script.js
+++ script.js
@@ -171,10 +171,12 @@ const thanks = document.querySelector(".content7")
 const nam = document.getElementById("name")
 const phone = document.getElementById("phone")
 const time = document.getElementById("time")
+const email = document.getElementById("email")
+
 
 const submit = document.querySelector(".submit")
 submit.addEventListener("click",()=>{
-      if(nam.value!=="" && phone.value !=="" && time.value !==""){
+      if(nam.value!=="" && phone.value !=="" && time.value !=="" && email.value !== ""){
     
          location.href="thanks.html"
     }
